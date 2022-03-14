7c4c56a3947f75e9d521795c6b2a4049a480e494
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-14 10:07:54 +0900

    first commit

---
+++ src/main/java/com/example/EcPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcPizzaApplication.class, args);
+	}
+
+}
