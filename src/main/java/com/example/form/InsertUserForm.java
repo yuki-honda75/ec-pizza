8237f4a8b1c7995947dd78d56c05ac5955a2b07d
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-14 11:51:30 +0900

    InsertUserFormにバリデーション追加

--- src/main/java/com/example/form/InsertUserForm.java
+++ src/main/java/com/example/form/InsertUserForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * 
  * @author hondayuki
@@ -7,17 +12,29 @@ package com.example.form;
  */
 public class InsertUserForm {
 	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
 	private String name;
 	/** Eメール */
+	@NotBlank(message = "名前を入力してください")
+	@Email(message = "メールアドレスの形式が不正です")
 	private String email;
-	/** パスワード */
-	private String password;
 	/** 郵便番号 */
+	@NotBlank(message = "郵便番号を入力してください")
+	@Pattern(regexp = "^[0-9]{3}-[0-9]{4}$", message = "郵便番号はXXX-XXXXの形式で入力してください")
 	private String zipcode;
 	/** 住所 */
+	@NotBlank(message = "住所を入力してください")
 	private String address;
 	/** 電話番号 */
+	@NotBlank(message = "電話番号を入力してください")
+	@Pattern(regexp = "^[0-9]{2,4}-[0-9]{2,4}-[0-9]{4}$", message = "電話番号はXXXX-XXXX-XXXXの形式で入力してください")
 	private String telephone;
+	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 8, max = 16, message = "パスワードは８文字以上１６文字以内で設定してください")
+	private String password;
+	/** 確認用パスワード */
+	private String confirmPass;
 	
 	public String getName() {
 		return name;
@@ -56,12 +73,19 @@ public class InsertUserForm {
 		this.telephone = telephone;
 	}
 	
+	public String getConfirmPass() {
+		return confirmPass;
+	}
+	public void setConfirmPass(String confirmPass) {
+		this.confirmPass = confirmPass;
+	}
+	
 	@Override
 	public String toString() {
 		return "InsertUserForm [name=" + name + ", email="
-				+ email + ", password=" + password
-				+ ", zipcode=" + zipcode + ", address="
-				+ address + ", telephone=" + telephone
-				+ "]";
+				+ email + ", zipcode=" + zipcode
+				+ ", address=" + address + ", telephone="
+				+ telephone + ", password=" + password
+				+ ", confirmPass=" + confirmPass + "]";
 	}
 }
