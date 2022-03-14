2c029c347cf9858b32285729aac60d36d7ae418d
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-14 11:59:12 +0900

    新規登録画面表示とhtmlを編集

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author hondayuki
+ *
+ */
+@Controller
+@RequestMapping("")
+public class UserController {
+	
+	@RequestMapping("/toRegister")
+	public String toRegister() {
+		return "register_user";
+	}
+}
