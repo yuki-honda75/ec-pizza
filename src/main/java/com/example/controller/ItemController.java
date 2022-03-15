39f2203ef43f8f6ce642da5f154a283222b9bdd8
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-03-15 10:47:01 +0900

    商品一覧画面作成

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,7 +1,14 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
+import com.example.service.ItemService;
 
 /**
  * 
@@ -11,9 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+	@Autowired
+	private ItemService itemService;
+	
 	//仮マッピング
 	@RequestMapping("/showList")
-	public String showList() {
+	public String showList(Model model) {
+		List<Item> itemList = itemService.showList();
+		model.addAttribute("itemList", itemList);
+		
 		return "item_list_pizza";
 	}
 }
